bdf34ecd2a05d0cb4ca134fe9f06b0e80f97dcd9
Matheus Littig <littig.works@gmail.com>
2021-09-16 22:50:00 -0400

    refactor: change axios to just display fake api data

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
-  // process.env.NODE_ENV === 'development'
-  //   ? 'http://localhost:3000/api'
-  //   : 'https://finances-web-client.vercel.app/',
+  baseURL:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3000/api'
+      : 'https://finances-web-client.vercel.app/api',
 });
